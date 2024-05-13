932c13078f7120b9eebc400c989886b978018098
RishEverec <117337128+rish4522@users.noreply.github.com>
2024-05-13 13:15:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const main = document.getElementById("section");
+    const form = document.getElementById("form");
+    const search = document.getElementById("query");
+
+    const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ad07acf4f4bb2d90b9032f092588c280&page=1';
+    const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+    const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=ad07acf4f4bb2d90b9032f092588c280&query=";
+
+    returnMovies(APILINK);
+
+    function returnMovies(url) {
+        fetch(url)
+        .then(res => res.json())
+        .then(function(data){
+            console.log(data.results);
+            data.results.forEach(element => {
+                const div_card = document.createElement('div');
+                div_card.setAttribute('class', 'card');
+
+                const div_row = document.createElement('div');
+                div_row.setAttribute('class', 'row');
+
+                const div_column = document.createElement('div');
+                div_column.setAttribute('class', 'column');
+
+                const image = document.createElement('img');
+                image.setAttribute('class', 'thumbnail');
+                image.setAttribute('id', 'image');
+
+                const title = document.createElement('h3');
+                title.setAttribute('id', 'title');
+                title.setAttribute('class', 'league-spartan-font')
+
+                title.innerHTML = `${element.title}`;
+                image.src = IMG_PATH + element.poster_path;
+                div_card.appendChild(image);
+                div_card.appendChild(title);
+                div_column.appendChild(div_card);
+                div_row.appendChild(div_column);
+
+                main.appendChild(div_row);
+            });
+        });
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        main.innerHTML = '';
+
+        const searchItem = search.value;
+
+        if (searchItem) {
+            returnMovies(SEARCHAPI + searchItem);
+            search.value = "";
+        }
+    });
+});
